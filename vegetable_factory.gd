a3aa9dd7d446a865b0386bf95da866bbbeb6d3ec
Ryan <ryancculbertsonfaegre@gmail.com>
2024-05-06 18:53:25 -0700

    Add Vegetable Factory Add a mechanism to create instances of vegetables TODO: fix the collision so that vegetables actually bounce off each other. ;_;

---
+++ vegetable_factory.gd
@@ -0,0 +1,24 @@
+extends Node2D
+@export var vegetable_scene: PackedScene = preload("res://vegetable.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _unhandled_input(event):
+	if event.is_echo():
+		return
+	if event is InputEventMouseButton and event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			spawn(get_global_mouse_position())
+
+
+func spawn(spawn_global_position):
+	var instance = vegetable_scene.instantiate()
+	instance.global_position = spawn_global_position
+	add_child(instance)
